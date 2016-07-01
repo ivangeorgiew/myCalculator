68a10e8061ede03f7722b9da01dd45b1688d571b
Ivan Georgiev <ivan_georgiew@yahoo.com>
2016-07-01 18:10:01 +0300

    Finished the project

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,128 @@
 $(document).ready(function() {
-  var store = '';
-  var ans = 0;
-  $('.nums').on('click', function(e){
-    var value = e.target.innerText;
-    $('.output').append('haha');
+  var store = '',
+      ans = '',
+      equalWasClicked = false,
+      dotWasClicked = false,
+      operatorWasClicked = false;
+
+  $('.ac').on('click', function(){
+    dotWasClicked = false;
+
+    $('.output').empty();
+    store = '';
+    ans = '';
+
+    $('.store').empty().append(store);
+  });
+
+  $('.c').on('click', function(){
+    dotWasClicked = false;
+    
+    if(store !== '')
+      store = store.slice(0, -($('.output').text().length));
+
+    $('.output').empty();
+
+    $('.store').empty().append(store);
+  });
+  
+  $('.nums').on('click', function(){
+    operatorWasClicked = false;
+    var value = $(this).text(); 
+
+    if(equalWasClicked){
+      $('.output').empty();
+      equalWasClicked = false;
+    }
+    
+    if(ans === '' || $('.output').text() !== ans){
+      store += value;
+      $('.output').append(value);
+    }
+
+    $('.store').empty().append(store);
+  });
+
+  $('.operators').on('click', function(){
+    dotWasClicked = false;
+    var value = $(this).text();
+    
+    if($('.output').is(':empty') === false){
+      if(store !== '' && !operatorWasClicked){
+
+        if($('.output').text().slice(0, 1) === '-')
+          store += ')';
+
+        store += value;
+        $('.output').empty();
+        operatorWasClicked = true;
+      }
+    }
+    else{
+      store += '(' + value;
+      $('.output').append(value);
+    }
+
+    $('.store').empty().append(store);
+  });
+
+  $('.ans').on('click', function(){
+    dotWasClicked = false;
+
+    if(ans !== '' && $('.output').is(':empty')){
+      operatorWasClicked = false;
+      equalWasClicked = false;
+
+      if(ans.toString().slice(0, 1) === '-'){
+        ans = '(' + ans + ')';
+      }
+
+      $('.output').append(ans);
+      store += ans;
+      //ans = '';
+    }
+
+    $('.store').empty().append(store);
+  });
+
+  $('.dot').on('click', function(){
+    var value = $(this).text();
+
+    if(store !== '' && dotWasClicked === false && !$('.output').text().slice(-1)){
+      store += value;
+      $('.output').append(value);
+
+      operatorWasClicked = true;
+      dotWasClicked = true;
+    }
+
+    $('.store').empty().append(store);
+  });
+
+  $('.equal').on('click', function(){
+    operatorWasClicked = false;
+    dotWasClicked = false;
+
+    var lastChar = store.slice(-1);
+    var syms = ['*', '/', '+', '-', '%'];
+    var notEq = false;
+    for(var i = 0; i < syms.length; i++){
+      if(lastChar === syms[i])
+        notEq = true;
+    }
+
+    if(notEq){
+      store = store.slice(0, -1);
+    }
+
+    if(equalWasClicked === false){
+      $('.output').empty().append(eval(store));
+      ans = eval(store);
+      store = '';
+      equalWasClicked = true;
+    }
+    
+    
+    $('.store').empty().append(store);
   });
 });
